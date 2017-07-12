8f875fa1a5cf7c9e330bad4624de762d75f739f7
Adam Klepacz <adamklepacz92@gmail.com>
2017-07-12 21:34:03 +0200

    update

--- index.php
+++ index.php
@@ -404,7 +404,7 @@
                   </div><!-- /.col-md-12 -->
                 </div><!-- /.col-md-6 -->
                 <div class="col-md-6">
-                  <form method="post" action="form.php" class="contact-form" id="contact_form">
+                  <form method="post" class="contact-form" id="contact_form">
                     <div class="col-md-12">
 
                       <!-- name -->
@@ -497,5 +497,40 @@
       </footer><!-- /footer-->
 
     </div><!-- /.page-wrapper -->
+    
+    <?php
+			if (count($_POST))
+			{
+				////////// USTAWIENIA //////////
+				$email = 'myjniabukownica@gmail.com';	// Adres e-mail adresata
+				$subject = 'Zapytanie o usługę detailingu';	// Temat listu
+				$message = 'Wiadomość została wysłana!';	// Komunikat
+				$error = 'Wystąpił błąd podczas wysyłania formularza';	// Komunikat błędu
+				$charset = 'UTF-8';	// Strona kodowa
+				//////////////////////////////
+
+				$head =
+					"MIME-Version: 1.0\r\n" .
+					"Content-Type: text/plain; charset=$charset\r\n" .
+					"Content-Transfer-Encoding: 8bit";
+				$body = '';
+				foreach ($_POST as $name => $value)
+				{
+					if (is_array($value))
+					{
+						for ($i = 0; $i < count($value); $i++)
+						{
+							$body .= "$name=" . (get_magic_quotes_gpc() ? stripslashes($value[$i]) : $value[$i]) . "\r\n";
+						}
+					}
+					else $body .= "$name=" . (get_magic_quotes_gpc() ? stripslashes($value) : $value) . "\r\n";
+				}
+				echo mail($email, "=?$charset?B?" . base64_encode($subject) . "?=", $body, $head) ? $message : $error;
+			}
+			else
+			{
+				echo '<p><b>Nie wysłano maila!</p>';
+			}
+		?>
   </body>
 </html>
